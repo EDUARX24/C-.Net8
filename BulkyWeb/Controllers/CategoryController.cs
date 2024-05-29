0e2d5f7c09f37e3028bd9bc43c6482999ae73b22
Alessandro013 <brioneszapata13@gmail.com>
2024-05-29 11:49:10 -0600

    first commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) // ApplicationDbContext is from program.cs service
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj); //save in ToList
+            _db.SaveChanges();// save in database
+            return RedirectToAction("Index"); //return RedirectToAction("Index", "Category"); you can redirect to other controller
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } //for default is primary or can write [key]
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
